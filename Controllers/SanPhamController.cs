746683015872e2bfec632bd19da896af3e69261d
TungZi <accgenshincuavideptrai@gmail.com>
2025-10-27 19:06:57 +0700

    ThemChiTietSanPham

---
+++ Controllers/SanPhamController.cs
@@ -0,0 +1,32 @@
+﻿using LTW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LTW.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        DBConnect db = new DBConnect();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ChiTiet(int id)
+        {
+            var sp = new SanPhamData();
+            var lstsp = sp.dsSanPham.ToList();
+            var ctsp = lstsp.FirstOrDefault(s => s.SanPhamID == id);
+            return View(ctsp);
+        }
+        public PartialViewResult CauHinhPartial(int id)
+        {
+            ChiTietCauHinhData chData = new ChiTietCauHinhData();
+            var ch = chData.GetBySanPhamID(id);
+
+            return PartialView("CauHinhPartial", ch);
+        }
+    }
+}
